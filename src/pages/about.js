36d2a1a9703b159fb14feb8c8e2d26a23dcfc205
samphillippo <samphillippo1@gmail.com>
2024-04-28 17:23:55 -0400

    about me laid out

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,32 +1,41 @@
 import * as React from 'react'
-import { title, text } from './styles.module.css'
+import { aboutMeContainer, profileSection, linksSection, experienceSection, missionStatement } from './about.module.css'
 import Navbar from '../components/navbar'
 import Seo from '../components/seo'
+import { StaticImage } from 'gatsby-plugin-image';
 
 const AboutPage = () => {
   return (
     <div>
       <Navbar />
-      <h1 className={title}>About Me</h1>
-      <p className={text}>I'm currently a Senior at Northeastern University studying Computer Science
-      with a Concentration in Artificial Intelligence, set to graduate with highest honors in May 2024.
-      I pride myself on being highly adaptable, and highly motivated, and I use these skills to maximize
-      my learning in every endeavor.</p>
-
-     <p className={text}> My main areas of interest are computer vision and machine learning, which I've been able to explore
-      in depth through both my work and academic experiences. I'm currently working on furthering my knowledge
-      of deep-learning practices, and stay up to date with current research into the field.</p>
-
-      <p className={text}>In my personal time, I've been working on a 3D sandbox game in OpenGL to gain further graphical skills,
-      and a prompt-based bibliography tool to improve my understanding of Natural Language Processing.
-      Outside of software, I am passionate about writing, playing, and listening to music, as well as experiencing
-      new cultures through travel and conversation.</p>
-
-      <p className={text}>If you share any of these interests, please don't hesitate to message me!</p>
-
+      <div className={aboutMeContainer}>
+        <div className={profileSection}>
+          <h1>About Me</h1>
+          <StaticImage src="../../images/samphil.png" alt="Profile Picture" />
+          <p className={missionStatement}>
+            Mission Statement here
+          </p>
+        </div>
+        <div className={linksSection}>
+          <h2>Links</h2>
+          <ul>
+            <li><a href="https://www.linkedin.com">LinkedIn</a></li>
+            <li><a href="https://github.com">GitHub</a></li>
+            <li><a href="/resume.pdf">Resume</a></li>
+          </ul>
+        </div>
+        <div className={experienceSection}>
+          <h2>Work Experience</h2>
+          <p>Work experience here.</p>
+        </div>
+        <div>
+          <h2>Favorite Things</h2>
+          <p>Favorite things here.</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export const Head = () => <Seo title="About" />
 
